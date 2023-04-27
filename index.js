34ce6bd2fe8805f45ddd0c513aedbceebcd553f7
Talentos C08100O <lorenzodeckmanndealmeida@gmail.com>
2023-04-27 08:53:12 -0300

    Exercicios

---
+++ index.js
@@ -0,0 +1,75 @@
+console.log("hello :)")
+//comentariu
+
+//const nomeSobrenome = prompt ("digite seu nome completo?")
+
+//const idade = prompt ("digite sua idade?")
+
+//const estudante = prompt("Você é estudande?digite ''sou'' ou ''não sou''")
+
+//console.log("Olá, meu nome é",nomeSobrenome,"eu tenho",idade,"e eu",estudante,"estudante." )
+//const got = "game of thrones"
+//const tempoeradasDeGot = 8
+//console.log(typeof got)
+//console.log(typeof tempoeradasDeGot)
+// let anoNascimento = prompt(("digite sua data de nascimento"))
+ //const idade = 15 - anoNascimento
+ //idade= idade + 10
+
+ //console.log("sua idade daqui a 10 anos é",idade)
+ //let a = 10
+//let b = 10
+
+//console.log(b)
+
+//b = 5
+//console.log(a, b)
+
+//Esse comando troca o valor de uma variavel
+
+//let a = 10
+//let b = 20
+//c = a
+//b = c
+//a = b
+
+//console.log(a, b, c)
+
+
+//let nome 
+//nome = prompt("Digite seu nome?")
+
+//let idade 
+//idade = prompt("Digite sua idade?")
+
+//console.log ("Olá",nome,",sua idade é",idade)
+
+
+//let horarioAula
+//horarioAula = prompt("Qual seu turno escolar?digite manhã,tarde ou noite.")
+
+//let transporteIda
+//transporteIda = prompt("Qual transporte você usa para ir á escola?digite carro,caminhada,bicicleta ou onibûs")
+
+//let transporteVolta
+//transporteVolta = prompt("Qual transporte você usa para voltar pra casa?Digite carro,caminhada,bicicleta ou onibûs")
+
+//console.log(horarioAula,transporteIda,transporteVolta )
+
+///let cavaloReserva = 23
+///let cavalo = 40
+///let cachorro = 23
+
+///console.log ("numero",cavagit clone loReserva,"numero",cavalo,)
+
+// Aqui faremos uma lógica para trocar os valores
+///cavaloReserva = cavalo
+///cavalo = cavaloReserva
+///Cavalo = cachorro
+
+// Depois de trocados, teremos o seguinte resultado:
+///console.log("O novo valor de",cachorro,"é",cavalo ) 
+///console.log ("numero",cavalo,"numero",cachorro)
+
+//adicionar outra variante=cavaloReserva = 40
+
